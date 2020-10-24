ecf17eb33d5baa7377d55ede91002985cff8831c
matt-blackbourn <matt.blackie114@gmail.com>
2020-10-25 11:13:09 +1300

    refactor

--- public/yahtzee.js
+++ public/yahtzee.js
@@ -1,159 +1,59 @@
-document.addEventListener('DOMContentLoaded', startGame)
+const rollsDisplay = document.querySelector("#rolls")
+const totalDisplay = document.querySelector("#total")
 
-let diceButtons = document.querySelectorAll(".dice")
-let rollsDisplay = document.querySelector("#rolls")
-let totalDisplay = document.querySelector("#total")
 let rollsRemaining = 3
-let dice = []
-let tempHash = {}
+let dice = [
+   {value: null, keep: false},
+   {value: null, keep: false},
+   {value: null, keep: false},
+   {value: null, keep: false},
+   {value: null, keep: false}
+]
 
-function startGame(){
-   //event listeners
-   for(let i = 0; i < diceButtons.length; i++){
-      diceButtons[i].addEventListener("click", function(e){
-         if(rollsRemaining < 3 && rollsRemaining > 0){
-            !dice[i].keep ? dice[i].keep = true : dice[i].keep = false
-            e.target.classList.toggle("keep")
-         }
-      })
-   }
-   //build dice array
-   for(let i = 0; i < 5; i++){
-      dice.push({keep: false, value: null})
-   }
+//ADD DICE EVENT LISTENERS
+const diceButtons = document.querySelectorAll(".dice")
+for(let i = 0; i < diceButtons.length; i++){
+   diceButtons[i].addEventListener("click", function(e){
+      if(rollsRemaining > 0 && rollsRemaining < 3) toggleKeepDice(i, e)
+   })
+}
+
+//ADD SCORING BUTTON EVENT LISTENERS
+const scoringOptions = document.querySelectorAll(".scoringOptions")
+for(let i = 0; i < scoringOptions.length; i ++){
+   scoringOptions[i].addEventListener("click", calculateTotal)
 }
 
 function roll(){
    if(rollsRemaining > 0){
-      for(let i = 0; i < dice.length; i++){
-         if(!dice[i].keep){
-            dice[i].value = (Math.floor(Math.random()*6)+1)
-            diceButtons[i].innerHTML = dice[i].value
-         } 
-      }
+      rollAvailableDice()
+      checkPossibleScores()
       rollsRemaining --
       rollsDisplay.innerHTML = rollsRemaining
    }
-  
-   checkAvailableScores()
 }
 
-function reset(){
+function nextPlayer(){
    if(rollsRemaining > 0){
-      return alert("Use all your rolls first!")
-   }
-   //reset dice array
-   for(let i = 0; i < dice.length; i++){
-      dice[i].value = null
-      dice[i].keep = false
-      diceButtons[i].innerHTML = ""
-      diceButtons[i].classList.remove("keep")
-   }
-   //reset rolls remaining
-   rollsRemaining = 3
-   rollsDisplay.innerHTML = rollsRemaining
-   totalDisplay.innerHTML = ""
-
-   resetScoringButtons()
-}
-
-function resetScoringButtons(){
-   let scoringOptions = document.querySelectorAll(".scoringOptions")
-   for(let i = 0; i < scoringOptions.length; i++){
-      scoringOptions[i].classList.remove("availableScoreLower")
-      scoringOptions[i].classList.remove("availableScoreUpper")
-      scoringOptions[i].classList.remove("availableScoreYahtzee")
-      scoringOptions[i].disabled = true
+      alert("Use all your rolls first!")
+   } else {
+      resetDiceArray()
+      resetRollsRemaining()
+      disableScoreButtons()
    }
 }
 
-function checkAvailableScores(){
-   resetScoringButtons()
-   tempHash = {}
-   for(let i = 0; i < dice.length; i++){
-      tempHash[dice[i].value] = (tempHash[dice[i].value] || 0) + 1  
-   }
-   let keys = Object.keys(tempHash)
-   //small straight test
-   if(keys.length >= 4){
-      if((keys[0] === "1" && keys[3] === "4") || 
-         (keys[0] === "2" && keys[3] === "5") ||
-         (keys[0] === "3" && keys[3] === "6") ||
-         (keys[1] === "3" && keys[4] === "6")){
-            let smallStraight = document.querySelector("#smallStraight")
-            smallStraight.disabled = false
-            smallStraight.classList.add("availableScoreLower")
-      }
-   }
-   //large straight test
-   if(keys.length === 5){
-      if((keys[0] === "1" && keys[4] === "5") ||
-         (keys[0] === "2" && keys[4] === "6")){
-            let largeStraight = document.querySelector("#largeStraight")
-            largeStraight.disabled = false
-            largeStraight.classList.add("availableScoreLower")
-         }
-   }
-   
-   for(let [key, value] of Object.entries(tempHash)){
-      if(key === "1"){
-         let ones = document.querySelector("#ones")
-         ones.disabled = false
-         ones.classList.add("availableScoreUpper")
-      }
-      if(key === "2"){
-         let twos = document.querySelector("#twos")
-         twos.disabled = false
-         twos.classList.add("availableScoreUpper")
-      }
-      if(key === "3"){
-         let threes = document.querySelector("#threes")
-         threes.disabled = false
-         threes.classList.add("availableScoreUpper")
-      }
-      if(key === "4"){
-         let fours = document.querySelector("#fours")
-         fours.disabled = false
-         fours.classList.add("availableScoreUpper")
-      }
-      if(key === "5"){
-         let fives = document.querySelector("#fives")
-         fives.disabled = false
-         fives.classList.add("availableScoreUpper")
-      }
-      if(key === "6"){
-         let sixes = document.querySelector("#sixes")
-         sixes.disabled = false
-         sixes.classList.add("availableScoreUpper")
-      }
-      if(value >= 3){
-         let threeOfKind = document.querySelector("#threeOfKind")
-         threeOfKind.disabled = false
-         threeOfKind.classList.add("availableScoreLower")
-      }
-      if(value >= 4){
-         let fourOfKind = document.querySelector("#fourOfKind")
-         fourOfKind.disabled = false
-         fourOfKind.classList.add("availableScoreLower")
-      }
-      if(value === 3 && Object.entries(tempHash).length === 2){
-         let fullHouse = document.querySelector("#fullHouse")
-         fullHouse.disabled = false
-         fullHouse.classList.add("availableScoreLower")
-      }
-      if(value === 5){
-         let yahtzee = document.querySelector("#yahtzee")
-         yahtzee.disabled = false
-         yahtzee.classList.add("availableScoreYahtzee")
-      }
-   }
-   let scoringOptions = document.querySelectorAll(".scoringOptions")
-   for(let i = 0; i < scoringOptions.length; i ++){
-      scoringOptions[i].addEventListener("click", total)
-   }
+function checkPossibleScores(){
+   disableScoreButtons()
+   const tempHash = buildTempDiceHash()
+   checkForNumbers(tempHash)
+   checkForRuns(tempHash)
+   checkForSmallStraight(tempHash)
+   checkForLargeStraight(tempHash)
 }
 
-function total(e){
+function calculateTotal(e){
+   const tempHash = buildTempDiceHash()
    let score = 0
    if(e.target.id === "ones") score = tempHash["1"]
    if(e.target.id === "twos") score = 2 * tempHash["2"]
@@ -173,3 +73,120 @@ function total(e){
       }
    totalDisplay.innerHTML = "Score is: " + score
 }
+
+//=============HELPER FUNCS=================
+
+function toggleKeepDice(i, e){
+   !dice[i].keep ? dice[i].keep = true : dice[i].keep = false
+   e.target.classList.toggle("keep")
+}
+
+function rollAvailableDice(){
+   for(let i = 0; i < dice.length; i++){
+      if(!dice[i].keep){
+         dice[i].value = (Math.floor(Math.random()*6)+1)
+         diceButtons[i].innerHTML = dice[i].value
+      } 
+   }
+}
+
+function resetDiceArray(){
+   for(let i = 0; i < dice.length; i++){
+      dice[i].value = null
+      dice[i].keep = false
+      diceButtons[i].innerHTML = ""
+      diceButtons[i].classList.remove("keep")
+   }
+}
+
+function resetRollsRemaining(){
+   rollsRemaining = 3
+   rollsDisplay.innerHTML = rollsRemaining
+   totalDisplay.innerHTML = ""
+}
+
+function disableScoreButtons(){
+   let scoringOptions = document.querySelectorAll(".scoringOptions")
+   for(let i = 0; i < scoringOptions.length; i++){
+      scoringOptions[i].classList.remove("availableScoreLower")
+      scoringOptions[i].classList.remove("availableScoreUpper")
+      scoringOptions[i].classList.remove("availableScoreYahtzee")
+      scoringOptions[i].disabled = true
+   }
+}
+
+function buildTempDiceHash(){
+   const tempDiceHash = {}
+   for(let i = 0; i < dice.length; i++){
+      tempDiceHash[dice[i].value] = (tempDiceHash[dice[i].value] || 0) + 1  
+   }
+   return tempDiceHash
+}
+
+function checkForNumbers(tempHash){
+   for(let key of Object.keys(tempHash)){
+      switch(key){
+         case '1': enableScoringButton('ones', 'Upper')
+         break
+         case '2': enableScoringButton('twos', 'Upper')
+         break
+         case '3': enableScoringButton('threes', 'Upper')
+         break
+         case '4': enableScoringButton('fours', 'Upper')
+         break
+         case '5': enableScoringButton('fives', 'Upper')
+         break
+         case '6': enableScoringButton('sixes', 'Upper')
+         break
+      }
+   }
+}
+
+function checkForRuns(tempHash){
+   for(let value of Object.values(tempHash)){
+      if(value >= 3) enableScoringButton('threeOfKind', 'Lower')
+      if(value >= 4) enableScoringButton('fourOfKind', 'Lower')
+      if(value === 5) enableScoringButton('yahtzee', 'Lower')
+      if(value === 3 && Object.entries(tempHash).length === 2){
+         enableScoringButton('fullHouse', 'Lower')
+      }
+   }
+}
+
+function checkForSmallStraight(tempHash){
+   let keys = Object.keys(tempHash)
+   if(keys.length >= 4){
+      if((keys[0] === "1" && keys[3] === "4") || 
+         (keys[0] === "2" && keys[3] === "5") ||
+         (keys[0] === "3" && keys[3] === "6") ||
+         (keys[1] === "3" && keys[4] === "6")){
+            enableScoringButton('smallStraight', 'Lower')
+      }
+   }
+}
+
+function checkForLargeStraight(tempHash){
+   let keys = Object.keys(tempHash)
+   if(keys.length === 5){
+      if((keys[0] === "1" && keys[4] === "5") ||
+         (keys[0] === "2" && keys[4] === "6")){
+            enableScoringButton('largeStraight', 'Lower')
+         }
+   }
+}
+
+function enableScoringButton(button, section){
+   const smallStraight = document.querySelector('#' + button)
+   smallStraight.disabled = false
+   smallStraight.classList.add('availableScore' + section)
+}
+
+// function sum(a, b) {
+//    return a + b;
+//  }
+
+// module.exports = {
+//    testForSmallStraight,
+//    testForLargeStraight,
+//    sum
+// }
