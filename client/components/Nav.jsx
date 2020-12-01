8af191f1bc05659a0c98b0c515349466a46bda05
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-01 18:31:10 +1300

    disable roll button with no rolls remaining

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -4,38 +4,42 @@ import { reduceRollsRemaining, enableCutScoreButton, restartGame } from '../acti
 import { rollAvailableDice } from '../utils/rollDiceLogic'
 
 const rollDice = props => {
-   if(props.rollsRemaining > 0){
-      rollAvailableDice(props)
-      props.dispatch(reduceRollsRemaining())
-      props.dispatch(enableCutScoreButton())
-   }
+  if (props.rollsRemaining > 0) {
+    rollAvailableDice(props)
+    props.dispatch(reduceRollsRemaining())
+    props.dispatch(enableCutScoreButton())
+  }
 }
 
 const restart = props => {
-  if(window.confirm('Are you sure you want to restart your game?')) { 
+  if (window.confirm('Are you sure you want to restart your game?')) {
     props.dispatch(restartGame())
   }
 }
 
 const Nav = props => {
-   return (
-      <div className="container flexCenter">
-         <button id="roll" onClick={() => rollDice(props)}>Roll</button>
-        <h4>You will score: <span>{props.rollScore.score}</span></h4>
-         <h4>Rolls remaining: <span>{props.rollsRemaining}</span></h4>
-         <button onClick={() => restart(props)}>Restart Game</button>
-      </div>
-   )
+  return (
+    <div className="container flexCenter">
+      <button
+        id="roll"
+        disabled={props.rollsRemaining === 0}
+        onClick={() => rollDice(props)}>
+        Roll</button>
+      <h4>Score: <span>{props.rollScore.score}</span></h4>
+      <h4>Rolls: <span>{props.rollsRemaining}</span></h4>
+      <button onClick={() => restart(props)}>Restart</button>
+    </div>
+  )
 }
 
 const mapState2Props = globalState => {
-   return {
-      dice: globalState.dice,
-      rollsRemaining: globalState.rollsRemaining,
-      tempHash: globalState.tempDiceHash,
-      rollScore: globalState.rollScore,
-      scoreCard: globalState.scoreCard
-   }
+  return {
+    dice: globalState.dice,
+    rollsRemaining: globalState.rollsRemaining,
+    tempHash: globalState.tempDiceHash,
+    rollScore: globalState.rollScore,
+    scoreCard: globalState.scoreCard
+  }
 }
 
 export default connect(mapState2Props)(Nav)
