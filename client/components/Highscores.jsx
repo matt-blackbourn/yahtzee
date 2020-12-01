d4e6a3e4e7114d3babd51c26ad5c69bacf1b22f2
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-01 17:20:24 +1300

    deep clone issue temp fixed

--- client/components/Highscores.jsx
+++ client/components/Highscores.jsx
@@ -10,7 +10,7 @@ function handleClick(props, event){
 function Hightscores(props) {
    return(
       <>
-         <h1>Highscores page!</h1>
+         <h1>ASH IS A GAYLORD</h1>
          <a href='' onClick={(event) => handleClick(props, event)}>Back to Yahtzee!</a>
       </>
    )
--- client/reducers/scoreCard.js
+++ client/reducers/scoreCard.js
@@ -22,7 +22,7 @@ const initialState = {
   grandTotal: {print: false, value: 0} 
 }
 
-const deepCloneInitialState = JSON.parse(JSON.stringify(initialState))
+// const deepCloneInitialState = JSON.parse(JSON.stringify(initialState))
 
 function reducer(state = initialState, action){
   let newState
@@ -65,7 +65,28 @@ function reducer(state = initialState, action){
       return newState
 
     case GAME_RESTARTED:
-      return deepCloneInitialState
+      // return deepCloneInitialState
+      return {
+        ones: {scored: false, value: 0},
+        twos: {scored: false, value: 0},
+        threes: {scored: false, value: 0},
+        fours: {scored: false, value: 0},
+        fives: {scored: false, value: 0},
+        sixes: {scored: false, value: 0},
+        total: {print: false, value: 0},
+        bonus: {print: false, value: 0},
+        upperTotal: {print: false, value: 0},
+        threeOfKind: {scored: false, value: 0},
+        fourOfKind: {scored: false, value: 0},
+        fullHouse: {scored: false, value: 0},
+        smStraight: {scored: false, value: 0},
+        lgStraight: {scored: false, value: 0},
+        yahtzee: {scored: false, value: 0},
+        chance: {scored: false, value: 0},
+        bonusYahtzee: {print: false, value: 0},
+        lowerTotal: {print: false, value: 0},
+        grandTotal: {print: false, value: 0} 
+      }
       
     default: 
       return state
