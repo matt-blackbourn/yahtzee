1369e0c35a6773a4740d561a3b0f926997483242
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-19 22:10:49 +1300

    register route accepts req.body

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.post('/register', (req, res) => {
-  res.json({message: 'route hit'})
-})
+router.post('/register', register)
+
+function register(req, res){
+  const {username, password} = req.body
+  res.send(`your name is ${username} and your password is ${password}`)
+}
 
 
 module.exports = router
