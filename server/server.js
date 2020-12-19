924d82d9505dca32a5e7083150c964e7a1dc6798
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-19 22:05:49 +1300

    add register route

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ const server = express()
 
 const scoresRoutes = require('./routes/scores')
 const cliRoutes = require('./routes/cli')
+const authRoutes = require('./routes/auth')
 
 server.use(express.json()) 
 server.use(express.static(path.join(__dirname, './public'))) 
 
 server.use('/api/scores', scoresRoutes)
 server.use('/cli', cliRoutes)
+server.use('/api/auth', authRoutes)
 
 module.exports = server
