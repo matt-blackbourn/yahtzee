190047f7f29e1cda57b121c58d346232dcc02340
matt-blackbourn <matt.blackie114@gmail.com>
2020-12-22 10:49:14 +1300

    post scores route protected

--- server/routes/scores.js
+++ server/routes/scores.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const verifyJwt = require('express-jwt') 
 
 const db = require('../db/scores')
 
@@ -14,18 +15,41 @@ router.get('/', (req, res) => {
    })
 })
 
-router.post('/', (req, res) => {
-  console.log(req.header('Authorization'))
-   const score = req.body
-   return db.addScore(score)
-    .then(() => db.getScores())
-    .then((scores) => res.json(scores))
-    .catch(err => {
-        console.log(err)
-        res.status(500).json({message: 'something broke'})
-    })
-})
+router.post('/', verifyJwt({secret: process.env.JWT_SECRET, algorithms: ['HS256']}), postScore)
+
+function postScore (req, res){
+  const score = req.body
+  return db.addScore(score)
+   .then(() => db.getScores())
+   .then((scores) => res.json(scores))
+   .catch(error => {
+       console.log(error)
+       res.status(500).json({message: 'something broke'})
+   })
+}
 
 module.exports = router
 
-// verifyJwt({secret: process.env.JWT_SECRET, algorithms: ['HS256']})
+
+// const h = {
+//   "ones": "0",
+//   "twos": "0",
+//   "threes": "0",
+//   "fours": "0",
+//   "fives": "0",
+//   "sixes": "0",
+//   "total": "0",
+//   "bonus": "0",
+//   "upperTotal": "0",
+//   "threeOfKind": "0",
+//   "fourOfKind": "0",
+//   "fullHouse": "0",
+//   "smStraight": "0",
+//   "lgStraight": "0",
+//   "yahtzee": "0",
+//   "chance": "0",
+//   "bonusYahtzee": "0",
+//   "lowerTotal": "0",
+//   "grandTotal": "0",
+//   "username": "hello"
+// }
